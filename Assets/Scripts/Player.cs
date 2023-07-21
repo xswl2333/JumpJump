a2c06cad73de27587e19c5c591c402da07cf595e
xswl2333 <3120778195@qq.com>
2023-07-21 23:18:29 +0800

    :sparkles: 添加盒子预制体,随机生成盒子

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,10 +9,21 @@ public class Player : MonoBehaviour
     private Rigidbody m_Rigidbody;
     public float fMaxForce = 500.0f;
     private float m_CurForce = 0.0f;
+
+    public GameObject Box=null;
+    public float fMinDistance = 1.2f;
+    public float fMaxDistance = 3.0f;
+    public float fMinHeight = 0.3f;
+    public float fMaxHeight = 2.0f;
+
+    private Vector3 m_Directiion = Vector3.forward;
+    private float m_Distance = 0.0f;
+    private float m_Height = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody=GetComponent<Rigidbody>();
+        GenerateBox();
     }
 
     // Update is called once per frame
@@ -50,4 +61,22 @@ public class Player : MonoBehaviour
         m_Rigidbody.AddForce(Vector3.up* m_CurForce);
         m_Rigidbody.AddForce(Vector3.forward* m_CurForce);
     }
+
+    private GameObject GenerateBox()
+    {
+        GameObject box = GameObject.Instantiate(Box);
+
+        m_Distance=Random.Range(fMinDistance, fMaxDistance);
+        m_Height=Random.Range(fMinHeight, fMaxHeight);
+        m_Directiion = Random.Range(0, 2) == 1 ? Vector3.forward : Vector3.left; //[0,2)
+
+        Vector3 pos=m_Directiion*m_Distance+transform.position;
+        pos.y = 0;
+        box.transform.position = pos;
+        box.transform.localScale = new Vector3(1, m_Height, 1);//Ö»Ëõ·Åy
+        box.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+        
+        return null;
+    }
+
 }
